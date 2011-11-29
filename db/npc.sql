bc59361fce1a9845632c50c4bf1f85ba30253ffa
Truong Nam Kien <truongnamkien@yahoo.com>
2011-11-29 15:07:18 +0700

    Add database structure of npc table

---
+++ db/npc.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 29, 2011 at 08:06 AM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `football`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `npc`
+--
+
+CREATE TABLE IF NOT EXISTS `npc` (
+  `npc_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `street_id` bigint(14) DEFAULT NULL,
+  `level` tinyint(4) DEFAULT '1',
+  PRIMARY KEY (`npc_id`),
+  UNIQUE KEY `street_id` (`street_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
